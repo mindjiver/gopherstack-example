07fe091cd7234199233972f67a6ab0d8c1a91b89
Peter Jönsson <peter.jonsson@klarna.com>
2014-04-01 12:35:38 +0200

    Update to latest gopherstack
    Always validate any SSL certificates inside the chain.

--- gopherstack-example.go
+++ gopherstack-example.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/mindjiver/gopherstack"
 	"os"
-	"time"
 )
 
 func main() {
@@ -25,32 +24,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	serviceofferingid := "e43adbcf-4004-40fb-a452-766aaf3a55f3"
-	templateid := "b34f2d7b-2bec-497e-a18e-06d0de94526e"
-	diskofferingid := "df0a9e58-5c1b-4943-8fdf-2469ce945b5b"
-	zoneid := "489e5147-85ba-4f28-a78d-226bf03db47c"
-	networkids := []string{"9ab9719e-1f03-40d1-bfbe-b5dbf598e27f"}
-	userdata := "#!ipxe\nchain http://10.4.128.74/cloudstack.ipxe\n"
+	// Always validate any SSL certificates in the chain
+	insecureskipverify := false
+	cs := gopherstack.CloudStackClient{}.New(apiurl, apikey, secret, insecureskipverify)
 
-	key_pair_name := "packer-key-pair"
-	displayname := "peter-packer-testing"
+	vmid := "19d2acfb-e281-4a13-8d62-e04ab501271d"
+	response, err := cs.ListVirtualMachines(vmid)
+	if err != nil {
+		fmt.Errorf("Error listing virtual machine: %s", err)
+		os.Exit(1)
+	}
 
-	cs := gopherstack.CloudStackClient{}.New(apiurl, apikey, secret)
+	if len(response.Listvirtualmachinesresponse.Virtualmachine) > 0 {
+		ip := response.Listvirtualmachinesresponse.Virtualmachine[0].Nic[0].Ipaddress
+		state := response.Listvirtualmachinesresponse.Virtualmachine[0].State
+		fmt.Printf("%s has IP : %s and state : %s\n", vmid, ip, state)
+	} else {
+		fmt.Printf("No VM with UUID: %s found\n", vmid)
+	}
 
-	cs.CreateSSHKeyPair(key_pair_name)
-	vmid, jobid, _ := cs.DeployVirtualMachine(serviceofferingid, templateid,
-		zoneid, "", diskofferingid, displayname, networkids,
-		key_pair_name,  "", userdata)
-	cs.WaitForAsyncJob(jobid, 2*time.Minute)
-
-	ip, state, _ := cs.ListVirtualMachines(vmid)
-	fmt.Printf("%s has IP : %s and state : %s", vmid, ip, state)
-
-	jobid, _ = cs.DetachIso(vmid)
-	cs.WaitForAsyncJob(jobid, 2*time.Minute)
-
-	ip, state, _ = cs.ListVirtualMachines(vmid)
-	fmt.Printf("%s has IP : %s and state : %s", vmid, ip, state)
-
-	cs.DeleteSSHKeyPair(key_pair_name)
 }
